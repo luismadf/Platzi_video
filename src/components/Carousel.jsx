225daa7283a2d751449ffd33de7c54a070928fb1
Luis De Freites <luisdefreites120@gmail.com>
2020-12-03 11:02:38 +0100

    Version 1.0

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable jsx-quotes */
+import React from "react";
+import "../assets/styles/components/Carousel.scss";
+
+const Carousel = ({ children }) => (
+  <div className="carousel">
+    <div className="carousel__container">{children}</div>
+  </div>
+);
+
+export default Carousel;
---
+++ src/containers/App.jsx
@@ -0,0 +1,51 @@
+/* eslint-disable jsx-quotes */
+/* eslint-disable quotes */
+import React, { useState, useEffect } from "react";
+import Header from "../components/Header";
+import Search from "../components/Search";
+import Categories from "../components/Categories";
+import Carousel from "../components/Carousel";
+import CarouselItem from "../components/CarouselItem";
+import Footer from "../components/Footer";
+import "../assets/styles/App.scss";
+import initialInitialState from "../hooks/useInitialState";
+import useInitialState from "../hooks/useInitialState";
+
+const API = "http://localhost:3000/initialState";
+
+const App = () => {
+  const initialState = useInitialState(API);
+  return (
+    <div className="App">
+      <Header />
+      <Search />
+      {initialState.mylist.length > 0 && (
+        <Categories title="Mi Lista">
+          <Carousel>
+            <CarouselItem />
+          </Carousel>
+        </Categories>
+      )}
+
+      <Categories title="Tendencias">
+        <Carousel>
+          {initialState.trends.map((item) => (
+            <CarouselItem key={item.id} {...item} />
+          ))}
+        </Carousel>
+      </Categories>
+
+      <Categories title="Originales de Platzi Video">
+        <Carousel>
+          {initialState.originals.map((item) => (
+            <CarouselItem key={item.id} {...item} />
+          ))}
+        </Carousel>
+      </Categories>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
